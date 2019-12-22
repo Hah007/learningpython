570a9f30451294d0f34cd7e1946077d5eec01054
hah007 <hah007@126.com>
2019-12-22 21:25:50 +0800

    he

--- 和风天气预报.py
+++ 和风天气预报.py
@@ -1,8 +1,9 @@
 import requests
 import re,time
+import json
 
-
-item = 'zhengzhou'
+item = 'zhengzhou,henan'
+#,后面的可选
 url = 'https://free-api.heweather.com/s6/weather/forecast?location=%s&key=a46fd5c4f1b54fda9ee71ba6711f09cd'%(item)
 #now	实况天气
 #forecast	3-10天预报  
@@ -13,7 +14,10 @@ time.sleep(2)
 #使用json格式获取数据
 datalist = res.json()
 #获取第一条城市信息
+#只查了一个城市时没有意义这句话
 data = datalist['HeWeather6'][0]
+# print(json.dumps(datalist, sort_keys=True, indent=4, separators=(', ', ': ')))
+
 for i in range(3):
     #输出城市信息
     print('城市：',data['basic']['location'])
@@ -22,4 +26,5 @@ for i in range(3):
     print('温度：',data['daily_forecast'][i]['tmp_min'],' ~ ',data['daily_forecast'][i]['tmp_max'])
     print('天气：',data['daily_forecast'][i]['cond_txt_d'],' ~ ',data['daily_forecast'][i]['cond_txt_n'])
     print(data['daily_forecast'][i]['wind_dir'],data['daily_forecast'][i]['wind_sc'],"级")
+    print('生活指数:',data['lifestyle']['txt'])
     print("-"*60)
---
+++ 爬取菜谱最终版.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import requests
+# 引用requests库
+import csv
+from bs4 import BeautifulSoup
+# 引用BeautifulSoup库
+
+def get_content(url):
+    try:
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0'
+        response = requests.get(url,headers={'User-Agent':user_agent})
+        response.raise_for_status()     # 如果返回的状态码不是200，则抛出异常
+        # response.encoding = response.apparent_encoding      # 根据响应信息判断网页的编码格式，便于response.text知道如何解码
+        #需要研究
+    except Exception as e:
+        print('爬取错误')
+    else:
+        print('爬取成功')
+        return response.text
+
+def parser_content(content):
+    bs_foods = BeautifulSoup(content,'html.parser')
+
+    # 解析数据
+    list_foods = bs_foods.find_all('div',class_='info pure-u')
+    # 查找最小父级标签
+
+    list_all = []
+    # 创建一个空列表，用于存储信息
+
+    for food in list_foods:
+
+        tag_a = food.find('a')
+        # 提取第0个父级标签中的<a>标签
+        name = tag_a.text[17:-13]
+        # 菜名，使用[17:-13]切掉了多余的信息
+        URL = 'http://www.xiachufang.com'+tag_a['href']
+        # 获取URL
+        tag_p = food.find('p',class_='ing ellipsis')
+        # 提取第0个父级标签中的<p>标签
+        ingredients = tag_p.text[1:-1]
+        # 食材，使用[1:-1]切掉了多余的信息
+        list_all.append([name,URL,ingredients])
+        # 将菜名、URL、食材，封装为列表，添加进list_all
+    print(list_all)
+    return list_all
+    
+
+def save_csv(list):
+    with open('caipu22.csv','w',encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow('热门菜谱')
+        writer.writerows(list)
+    print('csv文件保存成功......')
+
+if __name__ == '__main__':
+    url='http://www.xiachufang.com/explore/'
+    content = get_content(url)
+    food=parser_content(content)
+    save_csv(food)
