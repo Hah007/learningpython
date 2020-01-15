5e84d16f21f394571de334db0cd4aeae049b59ac
hah007 <hah007@126.com>
2020-01-15 15:39:25 +0800

    pp

--- 庆余年-CODE/weibo_comment.py
+++ 庆余年-CODE/weibo_comment.py
@@ -82,6 +82,8 @@ class weibo():
 		return True
 	'''模拟登陆'''
 	def login(self, username, password):
+		cookie = {"Cookie": "SINAGLOBAL=7952146920085.783.1551334476968; login_sid_t=78bfb027c26e9525735027a92ead049d; cross_origin_proto=SSL; Ugrow-G0=5c7144e56a57a456abed1d1511ad79e8; __guid=15428400.2141284791571530200.1578371819866.635; TC-V5-G0=595b7637c272b28fccec3e9d529f251a; WBStorage=42212210b087ca50|undefined; _s_tentry=www.so.com; wb_view_log=1920*10800.8999999761581421; Apache=8525263088903.128.1578371822432; ULV=1578371822438:30:1:1:8525263088903.128.1578371822432:1577502180321; wb_timefeed_1273999793=1; wb_view_log_1273999793=1920*10800.8999999761581421; UOR=tech.sina.com.cn,widget.weibo.com,login.sina.com.cn; ALF=1609907972; SSOLoginState=1578371972; SCF=AneegFDOF-Lez9jor8WhAYiGhgtxSb9iMMvVUcFt1pNlTMmdNNQyLCnFByZA-pxUaW3oWrRE3DuV-0r0IrD_taA.; SUB=_2A25zEHvVDeRhGedM7FEY-SfLwj-IHXVQZOodrDV8PUNbmtANLW_7kW9NWEOedQfxYBFpN15ypVUmegJWJCeDQmLn; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWpy-RafG-_3gi_V60GNmzF5JpX5KzhUgL.Fo2ES0e41K.N1Ke2dJLoIEq7i--RiKn0i-2pi--fi-2Xi-24i--RiKn0i-2pi--fiKLhi-z7e5tt; SUHB=0rrK85HHNeLRnE; wvr=6; monitor_count=5; TC-Page-G0=b32a5183aa64e96302acd8febeb88ce4|1578371975|1578371848; webim_unReadCount=%7B%22time%22%3A1578372098324%2C%22dm_pub_total%22%3A0%2C%22chat_group_client%22%3A0%2C%22allcountNum%22%3A0%2C%22msgbox%22%3A2%7D"}
+
 		data = {
 				'username': username,
 				'password': password,
@@ -99,7 +101,7 @@ class weibo():
 				'hff': '',
 				'hfp': ''
 				}
-		res = self.session.post(self.login_url, headers=self.login_headers, data=data)
+		res = self.session.post(self.login_url, headers=self.login_headers, data=data,cookies = cookie)
 		if res.json()['retcode'] == 20000000:
 			self.session.headers.update(self.login_headers)
 			print('[INFO]: Account -> %s, login successfully...' % username)
@@ -118,12 +120,14 @@ class weibo():
 if __name__ == '__main__':
 	import argparse
 	parser = argparse.ArgumentParser(description="weibo comments spider")
-	parser.add_argument('-u', dest='username', help='weibo username', default='')
-	parser.add_argument('-p', dest='password', help='weibo password', default='')
+	parser.add_argument('-u', dest='username', help='weibo username', default='eq350')
+	parser.add_argument('-p', dest='password', help='weibo password', default='hellohh')
 	parser.add_argument('-m', dest='max_page', help='max number of comment pages to crawl(number<int> larger than 0 or all)', default=100)
-	parser.add_argument('-l', dest='link', help='weibo comment link', default='')
+	parser.add_argument('-l', dest='link', help='weibo comment link', default='https://weibo.com/6475144268/Ii1rDEN6q')
 	parser.add_argument('-t', dest='url_type', help='weibo comment link type(pc or phone)', default='pc')
 	args = parser.parse_args()
+
+	
 	wb = weibo()
 	username = args.username
 	password = args.password
